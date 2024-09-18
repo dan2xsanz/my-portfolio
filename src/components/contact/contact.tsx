59a62bd6156f5caed6596c906d7b5e1ff8ca17a0
danlessanz <danles.sanz@gmail.com>
2024-09-19 00:42:10 +0800

    apply email sending using emailjs

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -1,6 +1,8 @@
 import { darkModeStore, selectedScreenStore } from "../../store";
 import { useNavigate } from "react-router-dom";
 import { Footer } from "../footer";
+import emailjs from "emailjs-com";
+
 import { useEffect, useState } from "react";
 import {
   HeaderEnums,
@@ -28,8 +30,57 @@ export const Contact = () => {
   const [contactDetails, setContactDetails] =
     useState<ContactDetailsInterface>();
 
+  const [isStatusMessage, setStatusMessage] = useState<string | undefined>(
+    undefined
+  );
+
   // ROUTING NAVIGATION
   const navigate = useNavigate();
+
+  const onSubmitEmail = () => {
+    const serviceID = "service_y6dzqrd";
+    const templateID = "template_kdc7kt2";
+    const userID = "YM7gmbALaFNByqWRS";
+    let email = contactDetails && contactDetails.email;
+    let from_name = contactDetails && contactDetails.name;
+    let message = contactDetails && contactDetails.message;
+    emailjs
+      .send(serviceID, templateID, { email, message, from_name }, userID)
+      .then(
+        () => {
+          alert("Email sent successfully");
+        },
+        () => {
+          alert("Failed to send email");
+        }
+      );
+    // RESET THE FIELDS
+    setContactDetails({
+      name: "",
+      email: "",
+      message: "",
+    });
+    setStatusMessage(
+      "Note: Email is already sent, please wait a couple of minuites to send again. Thank you"
+    );
+  };
+
+  useEffect(() => {
+    if (
+      contactDetails?.name === "" ||
+      contactDetails?.email === "" ||
+      contactDetails?.message === "" ||
+      contactDetails?.email === undefined ||
+      contactDetails?.name === undefined ||
+      contactDetails?.message === undefined
+    ) {
+      setStatusMessage(
+        "Note: Please fill required fields first before sending email."
+      );
+    } else {
+      setStatusMessage(undefined);
+    }
+  }, [contactDetails]);
 
   // AUTO SCROLL ON TOP
   useEffect(() => {
@@ -54,70 +105,98 @@ export const Contact = () => {
         </div>
         <div className="contact-field-container">
           <input
+            key={"name"}
             type="text"
             className={
               mode === "white"
                 ? "input-name-style-dark"
                 : "input-name-style-light"
             }
-            placeholder="Name"
+            placeholder="* Name"
+            value={contactDetails && contactDetails?.name}
             onChange={(data) => {
               if (data.target.value) {
                 setContactDetails({
                   ...contactDetails,
-                  name: data.target.value.toString().trim(),
+                  name: data.target.value.toString(),
+                });
+              } else {
+                setContactDetails({
+                  ...contactDetails,
+                  name: "",
                 });
               }
             }}
           />
           <input
             type="text"
+            key={"email"}
             className={
               mode === "white"
                 ? "input-name-style-dark"
                 : "input-name-style-light"
             }
-            placeholder="Email"
+            placeholder="* Email"
+            value={contactDetails && contactDetails.email}
             onChange={(data) => {
               if (data.target.value) {
                 setContactDetails({
                   ...contactDetails,
-                  email: data.target.value.toString().trim(),
+                  email: data.target.value.toString(),
+                });
+              } else {
+                setContactDetails({
+                  ...contactDetails,
+                  email: "",
                 });
               }
             }}
           />
           <textarea
-            style={{ height: "100px", paddingTop: "10px" }}
+            rows={5}
+            cols={20}
+            key={"message"}
             className={
               mode === "white"
                 ? "input-name-style-dark"
                 : "input-name-style-light"
             }
-            placeholder="Message"
-            rows={5}
-            cols={20}
+            value={contactDetails && contactDetails?.message}
+            placeholder="* Message"
             onChange={(data) => {
               if (data.target.value) {
                 setContactDetails({
                   ...contactDetails,
-                  message: data.target.value.toString().trim(),
+                  message: data.target.value.toString(),
+                });
+              } else {
+                setContactDetails({
+                  ...contactDetails,
+                  message: "",
                 });
               }
             }}
+            style={{ height: "100px", paddingTop: "10px" }}
           />
         </div>
       </div>
       <div
         style={{
           marginTop: "20px",
+          display: "flex",
+          flexDirection: "column",
+          gap: "20px",
         }}
       >
-        <Button
-          size={ButtonSize.large}
-          label={"Send Message"}
-          onClick={() => console.log(contactDetails)}
-        />
+        {isStatusMessage ? (
+          <Label labelSize={LabelSize.MEDIUM} labelText={isStatusMessage} />
+        ) : (
+          <Button
+            size={ButtonSize.large}
+            label={"Send Message"}
+            onClick={onSubmitEmail}
+          />
+        )}
       </div>
       {/* FOOTER */}
       <Footer
